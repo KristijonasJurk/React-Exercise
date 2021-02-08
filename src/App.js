1bd06e4e51adf6be82b1d6e4ce285d239c90cc3a
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-02-08 22:28:59 +0200

    Added css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
 function App() {
   return (
-    <div className="App">
-      <h2>react</h2>
+    <div className='container'>
+      <h2>Advanced Tutorial</h2>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
