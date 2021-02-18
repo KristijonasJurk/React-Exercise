3a569d3d24e1ac640ac667e4ca1a397451a356d4
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-02-18 19:52:43 +0200

    add items using useReducer

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -2,13 +2,41 @@ import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
 import { data } from '../../../data';
 // reducer function
-
+const reducer = (state, action) => {
+  if (action.type === 'ADD_ITEM') {
+    const newItems = [...state.people, action.payload]
+    return {
+      ...state,
+      people: newItems,
+      isModalOpen: true,
+      modalContent: 'item added',
+    }
+  }
+  if (action.type === 'NO_VALUE') {
+    return { ...state, isModalOpen: true, modalContent: 'please enter value' };
+  }
+  throw new Error('no matching action type')
+};
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: ''
+}
 const Index = () => {
   const [name, setName] = useState('')
-  const [people, setPeople] = useState('')
-  const [showModal, setShowModal] = useState(false)
+  const [state, dispatch] = useReducer(reducer, defaultState)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name) {
+      const newItem = { id: new Date().getTime().toString(), name }
+      dispatch({ type: 'ADD_ITEM', payload: newItem })
+      setName('')
+    } else {
+      dispatch({ type: 'NO_VALUE' });
+    }
+  }
   return <>
-    {showModal && <Modal />}
+    {state.isModalOpen && <Modal modalContent={state.modalContent} />}
     <form onSubmit={handleSubmit} className='form'>
       <div>
         <input
@@ -17,6 +45,13 @@ const Index = () => {
       </div>
       <button type='submit'>add</button>
     </form>
+    {state.people.map((person) => {
+      return (
+        <div key={person.id}>
+          <h4>{person.name}</h4>
+        </div>
+      )
+    })}
   </>;
 };
 
