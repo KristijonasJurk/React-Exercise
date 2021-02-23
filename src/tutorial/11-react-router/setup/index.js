5e61ff2a1447188e167938142fccf9716e655270
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-02-23 20:13:39 +0200

    react router basics

--- src/tutorial/11-react-router/setup/index.js
+++ src/tutorial/11-react-router/setup/index.js
@@ -10,7 +10,17 @@ import Person from './Person';
 // navbar
 import Navbar from './Navbar';
 const ReactRouterSetup = () => {
-  return <h2>react router</h2>;
+  return <Router>
+    <Route exact path='/'>
+      <Home />
+    </Route>
+    <Route path='/about'>
+      <Home />
+    </Route>
+    <Route path='/people'>
+      <Home />
+    </Route>
+  </Router>;
 };
 
 export default ReactRouterSetup;
